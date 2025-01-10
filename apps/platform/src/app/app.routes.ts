52b1f8ef6f592e6404f72343dbbfb37d96c9e770
Lukáš Sukeník <lukas.sukenik@ibillboard.com>
2025-01-10 15:32:20 +0100

    refactor

--- apps/platform/src/app/app.routes.ts
+++ apps/platform/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { LoadChildrenCallback, Route } from '@angular/router';
+import { Route } from '@angular/router';
 import { PlatformHomeComponent } from './home/platform-home.component';
 import { TargetingComponentWrapper } from './domains/targeting/targeting-wrapper.component';
 import {InventoryComponentWrapper} from './domains/inventory/inventory-wrapper.component';
@@ -12,82 +12,29 @@ export const appRoutes: Route[] = [
   {
     path: 'targeting',
     component: TargetingComponentWrapper,
-    loadChildren: async () => {
-      try {
-        const module = await loadRemoteModule({
-          type: 'module',
-          remoteEntry: 'http://localhost:4206/remoteEntry.js',
-          exposedModule: './TargetingRemoteRoutingModule'
-        });
-
-        return module.TargetingRemoteRoutingModule;
-      } catch (error) {
-        console.error('Error loading remote micro-frontend:', error);
-
-        return null;
-      }
-    }
+    loadChildren: getRemoteExposedModule('http://localhost:4206/remoteEntry.js', 'TargetingRemoteRoutingModule')
   },
   {
     path: 'inventory',
     component: InventoryComponentWrapper,
-    loadChildren: async () => {
-      try {
-        const module = await loadRemoteModule({
-          type: 'module',
-          remoteEntry: 'http://localhost:4206/remoteEntry.js',
-          exposedModule: './InventoryRemoteRoutingModule'
-        });
-
-        return module.InventoryRemoteRoutingModule;
-      } catch (error) {
-        console.error('Error loading remote micro-frontend:', error);
-
-        return null;
-      }
-    }
+    loadChildren: getRemoteExposedModule('http://localhost:4206/remoteEntry.js', 'InventoryRemoteRoutingModule')
   }
 ];
 
+function getRemoteExposedModule(remoteEntry: string, exposedModule: string): any {
+  return async () => {
+    try {
+      const module = await loadRemoteModule({
+        type: 'module',
+        remoteEntry,
+        exposedModule: `./${exposedModule}`
+      });
 
+      return module[exposedModule];
+    } catch (error) {
+      console.error('Error loading remote micro-frontend:', error);
 
-// export const appRoutes: Route[] = [
-//   {
-//     path: '',
-//     component: PlatformHomeComponent,
-//   },
-//   {
-//     path: 'targeting',
-//     component: TargetingComponentWrapper,
-//     loadChildren: getLoadChildrenCallback(
-//       'http://localhost:4206/remoteEntry.js',
-//       './TargetingRemoteRoutingModule'
-//     )
-//   },
-//   {
-//     path: 'inventory',
-//     component: InventoryComponentWrapper,
-//     loadChildren: getLoadChildrenCallback(
-//       'http://localhost:4206/remoteEntry.js',
-//       './InventoryRemoteRoutingModule'
-//     )
-//   }
-// ];
-
-// function getLoadChildrenCallback(remoteEntry: string, exposedModule: string): any {
-//   return async () => {
-//     try {
-//       const module = await loadRemoteModule({
-//         type: 'module',
-//         remoteEntry,
-//         exposedModule
-//       });
-
-//       return module[exposedModule];
-//     } catch (error) {
-//       console.error('Error loading remote micro-frontend:', error);
-
-//       return null;
-//     }
-//   }
-// }
+      return null;
+    }
+  }
+}
